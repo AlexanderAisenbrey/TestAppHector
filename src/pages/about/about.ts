d3b0f73421c237c8ba7bf8d027d81775d8759644
SiLenT_Tz <shadow-jan@hotmail.de>
2018-07-25 15:57:33 +0200

    GPS implemented

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
 
 @Component({
   selector: 'page-about',
@@ -7,8 +9,23 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  longitude: number;
+  latitude: number;
+  altitude: number;
+  speed: number;
 
+  constructor(private geolocation: Geolocation, public navCtrl: NavController) {
+    this.getThePosition();
   }
-
+  getThePosition()  {
+    this.geolocation.getCurrentPosition().then((resp) =>  {
+      console.log("Start get Postition");
+      this.longitude = Math.round(resp.coords.longitude*100)/100;
+      this.latitude = Math.round(resp.coords.latitude*100)/100;
+      this.altitude = Math.round(resp.coords.altitude*100)/100;
+      this.speed = resp.coords.speed;
+    }).catch((error) => {
+      console.log("Error getting Location" , error);
+    });
+  }
 }
