6755fa8dd6b354d294694f9cdfbf8cd272753108
Jan Christoph <shadow-jan@hotmail.de>
2018-09-14 14:13:02 +0200

    added some beacon stuff to test

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,60 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {IBeacon} from '@ionic-native/ibeacon';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-about',
+  templateUrl: 'about.html'
 })
-export class ContactPage {
+export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  delegate:any;
+regions = [
+    this.ibeacon.BeaconRegion('beacon1', 'B9407F30-F5F8-466E-AFF9-25556B57FE6D', 1, 1),
+    this.ibeacon.BeaconRegion('beacon2', 'B9407F30-F5F8-466E-AFF9-25556B57FE6D', 2, 1),
+    this.ibeacon.BeaconRegion('beacon3', 'B9407F30-F5F8-466E-AFF9-25556B57FE6D', 3, 1),
+    this.ibeacon.BeaconRegion('beacon4', 'B9407F30-F5F8-466E-AFF9-25556B57FE6D', 4, 1)
+  ]
 
+  constructor(public navCtrl: NavController, private iBeacon: IBeacon) {
+    this.delegate = this.iBeacon.Delegate();
+    this.didStartMonitoringForRegion();
   }
 
+requestAlwaysAuthorization(){
+this.iBeacon.requestAlwaysAuthorization();
 }
+
+startRangingBeaconsInRegion(region){
+this.delegate.didRangeBeaconsInRegion()
+  .subscribe(
+    data => console.log('didRangeBeaconsInRegion: ', data),
+    error => console.error()
+  );
+}
+
+didStartMonitoringForRegion(){
+this.delegate.didStartMonitoringForRegion()
+  .subscribe(
+    data => console.log('didStartMonitoringForRegion: ', data),
+    error => console.error()
+  );
+}
+
+didEnterRegion(){
+this.delegate.didEnterRegion()
+  .subscribe(
+    data => {
+      console.log('didEnterRegion: ', data);
+    }
+  );
+}
+
+let beaconRegion = this.ibeacon.BeaconRegion('deskBeacon','F7826DA6-ASDF-ASDF-8024-BC5B71E0893E');
+
+this.ibeacon.startMonitoringForRegion(beaconRegion)
+  .then(
+    () => console.log('Native layer recieved the request to monitoring'),
+    error => console.error('Native layer failed to begin monitoring: ', error)
+  );
+
